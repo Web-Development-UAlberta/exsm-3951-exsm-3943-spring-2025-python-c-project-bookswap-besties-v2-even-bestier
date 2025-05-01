9ddb4a79d578cd3a69d612b36548445a0032576b
Diana C <dianasimply@protonmail.com>
2025-05-01 18:24:27 -0500

    revised .gitignore file

--- exsm3951_bookswap/authentication/models.py
+++ exsm3951_bookswap/authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Member(AbstractUser):
+    # Node: the email and password are inhertited from AbstractUser
+    first_name = models.TextField(max_length=255, blank=False, null=False)
+    last_name = models.TextField(max_length=255, blank=False, null=False)
+    address = models.TextField(max_length=255, blank=False, null=False)
+

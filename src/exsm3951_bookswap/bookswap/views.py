428b5cd59e9062e819a1606dcafd751fddefbd73
Justin Bertok <justinbertok@gmail.com>
2025-05-07 01:29:53 -0600

    add placeholder code to /bookswap/views.py

--- src/exsm3951_bookswap/bookswap/views.py
+++ src/exsm3951_bookswap/bookswap/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+# Placeholder code, feel free to replace all
+@login_required
+def library_view(request):
+   
+    my_books = []  
+    wishlist = []      
+    return render(request, "library/library.html", {
+        "my_books": my_books,
+        "wishlist": wishlist
+    })
 
+# Placeholder code, feel free to replace all
+@login_required
+def browse_books_view(request):
+   
+    books = [] 
+
+    return render(request, "browse/browse_books.html", {
+        "books": books
+    })

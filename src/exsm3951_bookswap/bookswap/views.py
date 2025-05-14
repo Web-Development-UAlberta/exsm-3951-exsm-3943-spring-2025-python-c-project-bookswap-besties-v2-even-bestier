230268f5aab0122ca649a118ef1bb84704a4bd39
Nancy Randall <nrandall@ualberta.ca>
2025-05-13 21:14:40 -0600

    moved form and view for profile page into the profile app

--- src/exsm3951_bookswap/bookswap/views.py
+++ src/exsm3951_bookswap/bookswap/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Book, BookListing, Review, WishList, Shipment, Swap, Transaction
 from .utils.google_books import get_cover_image, get_books_data
-from .forms import BookForm, CustomEditUserForm
+from .forms import BookForm
 from authentication.models import Member
 from notifications.models import Notification
 from django.contrib import messages
@@ -77,24 +77,7 @@ def book_create_from_search(request):
 
     return render(request, 'partials/book_form.html', {'form': form})
 
-@login_required
-def update_account(request, pk):
-    user = get_object_or_404(Member, pk=pk)
-    if request.method == 'POST':
-        post_data = request.POST.copy()
-        form = CustomEditUserForm(post_data, isinstance=user)
-        user.first_name = request.POST.get('first_name')
-        user.last_name = request.POST.get('last_name')
-        #finish entering fields here
 
-        messages.success(request, "Profile updated successfully!")
-        return redirect('library') #where should we redirect to?
-    else:
-        form = CustomEditUserForm(isinstance=user)
-        context = {
-            'form': form,
-        }
-    return render(request, 'profile/settings.html', context)
 
 
 # TODO: Create Book listing view (which will trigger notifications creation)
